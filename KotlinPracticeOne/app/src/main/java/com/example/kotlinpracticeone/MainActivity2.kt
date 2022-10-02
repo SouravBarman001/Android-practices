edfde4b4578a613772cb40323d1b50837364ea0b
Sourav <sourav.iit.nstu@gmail.com>
2022-10-02 10:41:25 +0600

    intent added

--- KotlinPracticeOne/app/src/main/java/com/example/kotlinpracticeone/MainActivity2.kt
+++ KotlinPracticeOne/app/src/main/java/com/example/kotlinpracticeone/MainActivity2.kt
@@ -1,11 +1,18 @@
 package com.example.kotlinpracticeone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main2.*
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        previousIntent.setOnClickListener(){
+            startActivity(Intent(this@MainActivity2,MainActivity::class.java))
+        }
     }
 }
